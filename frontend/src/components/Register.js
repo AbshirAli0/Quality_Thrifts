2e485cf91d991da5c2fd790e06af914318f0de70
AbshirAli0 <abshirali1001@gmail.com>
2024-09-27 15:54:48 -0400

    css to register added

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -21,14 +21,27 @@ const Register = () => {
   };
 
   return (
-    <div>
-      <h2>Register Here</h2>
-      <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={handleRegister}>Register</button>
+    <div className="flex items-center justify-center min-h-screen bg-custom-color">
+      <div className="bg-gray-800 p-8 rounded-lg shadow-md w-96">
+      <h2 className="text-2xl font-bold text-center text-white mb-4">Register Here</h2>
+      <input type="email" placeholder="Email" className="w-full p-2 mb-4 rounded" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" className="w-full p-2 mb-4 rounded" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleRegister}  className="w-full bg-green-500 text-white p-2 rounded hover:bg-green-600 mt-2">Register</button>
       {error && <p>{error}</p>}
+
+      </div>     
     </div>
   );
 };
-
+{/* <div className="flex items-center justify-center min-h-screen bg-custom-color">
+<div className="bg-gray-800 p-8 rounded-lg shadow-md w-96">
+<h2 className="text-2xl font-bold text-center text-white mb-4">Login</h2>
+<input type="email" placeholder="Email" className="w-full p-2 mb-4 rounded" value={email} onChange={(e) => setEmail(e.target.value)} />
+<input type="password" className="w-full p-2 mb-4 rounded"placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+<button onClick={handleLogin} className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600">Login</button>
+<h3 className="text-center text-white mt-4">If you don't have an account, click this button</h3>
+<button onClick={redirectToRegister} className="w-full bg-green-500 text-white p-2 rounded hover:bg-green-600 mt-2">Here</button>
+{error && <p className="text-red-500 text-center mt-2">{error}</p>}
+</div>   
+</div> */}
 export default Register;
