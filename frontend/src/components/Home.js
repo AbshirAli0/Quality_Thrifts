2acd4bcfdbcf035b2f6a2d41275256d55db7d148
AbshirAli0 <abshirali1001@gmail.com>
2024-09-28 16:03:32 -0400

    sign out but moved in login

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -38,15 +38,15 @@ const Home = () => {
 
   return (
     <div className="bg-custom-color text-white p-8 min-h-screen">
-      <p className="text-2x font-bold text-right">Signed in as: {currentUser?.email}</p>
+      <div className="text-2x font-bold text-right">
+      <p >Signed in as: {currentUser?.email}</p>
+      <button onClick = {handleSignOut} className='w-50 bg-red-600 text-white p-1 rounded hover:bg-red-900 mt-1'>Sign Out</button>
+      </div>
       <h2 className="text-5xl mt-4 text-center font-bold">Quality Thrifts</h2>
-
       <nav className="text-center mt-6 ">
         <a onClick={() => navigate('/')} className="nav-link text-white hover:text-gray-400 text-xl">Home</a>
         <a onClick={() => navigate('/cart')} className="nav-link text-white hover:text-gray-400 text-xl">Cart</a>
-        <a onClick={() => navigate('/about')}className="nav-link text-white hover:text-gray-400 text-xl">About</a>
-        <a onClick = {handleSignOut} className="nav-link text-white hover:text-gray-400 text-xl">Sign Out</a>
-        
+        <a onClick={() => navigate('/about')}className="nav-link text-white hover:text-gray-400 text-xl">About</a>        
       </nav>
 
       <div className="bg-gray-800 p-6 rounded-lg mt-4">
@@ -57,6 +57,7 @@ const Home = () => {
               <div className=" p-4 rounded-lg text-center" >
                 <h3 className="text-lg font-semibold">{item.brand}</h3>
                 <p className="mt-2">Price: ${item.price}</p>
+                <button className="w-full bg-green-600 text-white p-2 rounded hover:bg-green-500 mt-2">add to cart</button>
               </div>
             </div>
           ))}
